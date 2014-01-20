5192f611da5268778458b7a92fe5d529bd11c55c
Billy Vong <billyvg@gmail.com>
2014-01-20 12:57:34 -0800

    Add some python package files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='woopra',
+    version='0.1.0',
+    author='Antoine Chkaiban',
+    author_email='antoine@woopra.com',
+    url='https://github.com/Woopra/woopra-python-sdk',
+    packages=['woopra',],
+    license='LICENSE.txt',
+    description='Woopra Analytics Tracking API',
+)
