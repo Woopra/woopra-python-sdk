e4401ebe2f5fbe223e9465a720ffb1a6b6e26bd7
dev <jad.younan@gmail.com>
2016-12-22 12:40:04 -0800

    build info

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='woopra_tracker',
-    version='0.2.2',
+    version='0.3',
     author='Woopra',
     author_email='dev@woopra.com',
     url='https://github.com/Woopra/woopra-python-sdk',
-    packages=['woopra_tracker',],
+    packages=['woopra_tracker'],
     license='LICENSE.txt',
     description='Woopra Analytics Tracking API',
 )
